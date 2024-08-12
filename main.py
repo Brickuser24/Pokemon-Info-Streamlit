c61e6298e76fd9deca67c24b04ecff088784ca0e
Brickuser24 <151051795+Brickuser24@users.noreply.github.com>
2024-08-12 21:14:45 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,27 @@
 import streamlit as st
 import requests
+import csv
 
 with open("Pokedex.txt", "r") as f:
     read=f.read()
     options=read.split("\n")
+    f.close()
+
+with open("Type_Data.csv", encoding="utf8") as f:
+    cr = csv.reader(f)
+    next(cr)
+    Type_Data = {}
+    headers = ["", "Coverages", "Color", "Resistance", "Weakness", "Immunity"]
+    for row in cr:
+        d = {}
+        for value in range(len(row)):
+            if value == 0:
+                continue
+            if value == 2:
+                d[headers[value]] = row[value]
+            else:
+                d[headers[value]] = eval(row[value])
+        Type_Data[row[0]] = d
     f.close()
 
 coverage_options = {
@@ -36,8 +54,12 @@ try:
     types_string=":gray[Types:] "
     coverages = []
     coverage_string=":gray[Coverage Options:] "
+    imm,res,weak = [],[],[]
     for type_data in data["types"]:
         type = type_data["type"]["name"].capitalize()
+        imm.extend(Type_Data[type]["Immunity"])
+        res.extend(Type_Data[type]["Resistance"])
+        weak.extend(Type_Data[type]["Weakness"])
         types_string+=f'<span style="color:{coverage_options[type][1]}">{type}</span>'+', '
         for coverage in coverage_options[type][0]:
             if coverage not in coverages:
@@ -46,6 +68,29 @@ try:
     base_stats = {}
     for stat in data["stats"]:
         base_stats[stat["stat"]["name"]] = stat["base_stat"]
+    res_=[]
+    for r in res:
+        if r not in res_ and r not in weak and r not in imm:
+            res_.append(r)
+    weak_=[]
+    for w in weak:
+      if w not in res and w not in weak_ and w not in imm:
+          weak_.append(w)
+    imm_string=":gray[Immunities:] "
+    res_string=":gray[Resistances:] "
+    weak_string=":gray[Weaknesses:] "
+    for i in imm:
+        imm_string+=f'<span style="color:{coverage_options[i][1]}">{i}</span>'+', '
+    if len(imm)==0:
+        imm_string+=":gray[None]  "
+    for r in res_:
+        res_string+=f'<span style="color:{coverage_options[r][1]}">{r}</span>'+', '
+    if len(res_)==0:
+        res_string+=":gray[None]  "
+    for w in weak_:
+        weak_string+=f'<span style="color:{coverage_options[w][1]}">{w}</span>'+', '
+    if len(weak_)==0:
+        weak_string+=":gray[None]  "
     col1, col2 = st.columns(2)
     with col1:
         st.write(f':gray[{name} Info]')
@@ -66,7 +111,10 @@ try:
                 stat_int=f'<span style="color:DarkTurquoise">{stat_value}</span>'
             st.write(f":gray[{stat.title()}:] "+stat_int, unsafe_allow_html=True)
         st.write(coverage_string[0:-2:], unsafe_allow_html=True)
+        st.markdown(weak_string[0:-2:], unsafe_allow_html=True)
+        st.markdown(imm_string[0:-2:], unsafe_allow_html=True)
+        st.markdown(res_string[0:-2:], unsafe_allow_html=True)
     with col2:
         st.image(image_url, width=100)  
-except:
-    st.write("An error occured")
+except Exception as e:
+    st.write(e)
